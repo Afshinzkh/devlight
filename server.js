297fdabf2af94f1e33e2f3518a53321678075b56
afshinzkh <afshin.loni@gmail.com>
2019-11-27 09:08:29 +0100

    connect the database

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send(`App is Running ya'll`);
