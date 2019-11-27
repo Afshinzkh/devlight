25d40db02d60f87dc1b2751b797677976a3a488b
afshinzkh <afshin.loni@gmail.com>
2019-11-27 16:33:04 +0100

    add post/auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../model/Users');
-// @route   GET /auth
-// @desc    validate the token
-// @access  Public
+const bcrypt = require('bcrypt');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 
-router.get('/user', auth, async (req, res) => {
+// @route   GET /auth
+// @desc    Validate user token
+// @access  Public
+router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findOne({ id: req.user.id }).select(
       '-password -_id -__v'
@@ -17,5 +21,61 @@ router.get('/user', auth, async (req, res) => {
     res.status(500).send('Internal Server Error.');
   }
 });
+
+// @route   POST /auth
+// @desc    Authenticate user
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Valid Email Required.').isEmail(),
+    check('password', 'Password Required.').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials.' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials.' }] });
+      }
+
+      const id = user.id;
+      const payload = {
+        user: {
+          id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+  }
+);
 
 module.exports = router;
